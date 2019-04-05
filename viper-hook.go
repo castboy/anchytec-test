e665d7695595d6bc33736e7cb1017647c93901d6
castboy <482819895@qq.com>
2019-04-05 19:51:01 +0800

    add xorm and viper-hook

---
+++ viper-hook.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"github.com/mitchellh/mapstructure"
+	"github.com/shopspring/decimal"
+	"github.com/spf13/viper"
+	"reflect"
+)
+//self-define hookFunc to parse `float64` type  to `decimal.Decimal` type.
+
+func floatToDecimal() mapstructure.DecodeHookFunc {
+	return func(
+		f reflect.Type,
+		t reflect.Type,
+		data interface{}) (interface{}, error) {
+		if f.Kind() != reflect.Float64 {
+			return data, nil
+		}
+		if t != reflect.TypeOf(decimal.Decimal{}) {
+			return data, nil
+		}
+
+		// Convert it by parsing
+		dc := decimal.NewFromFloat(data.(float64))
+
+		return dc, nil
+	}
+}
+
+func decodeHookWithTag(hook mapstructure.DecodeHookFunc, tagName string) viper.DecoderConfigOption {
+	return func(c *mapstructure.DecoderConfig) {
+		c.DecodeHook = hook
+		c.TagName = tagName
+	}
+}
+
+func main() {
+	iViper := viper.New()
+
+	decodeHookFunc := floatToDecimal()
+	decoderConfigOption := decodeHookWithTag(decodeHookFunc, "json")
+
+	iViper.Set("salary", float64((100)))
+
+	var dc decimal.Decimal
+
+	err := iViper.UnmarshalKey("salary", &dc, decoderConfigOption)
+	fmt.Println(err, dc)
+
+	return
+}
---
+++ xorm.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/go-xorm/xorm"
+	"github.com/shopspring/decimal"
+	"log"
+)
+// decimal.Decimal type map table item type 'text' default. we can define certain type by set `xorm:"salary decimal(28,0) NOT NULL"`
+// by NoAutoCondition(true), we can get a clean query sql, like:
+// `SELECT `id`, `name`, `salary` FROM `person` WHERE id=1 AND name='wmg' LIMIT 1`, not
+// `SELECT `id`, `name`, `salary` FROM `account4` WHERE id=1 AND name='wmg' AND `salary`='100' LIMIT 1`
+
+type Person struct {
+	Id      int64  `xorm:"id"`
+	Name    string `xorm:"name"`
+	Salary  decimal.Decimal `xorm:"salary decimal(28,0) NOT NULL"`
+}
+
+var x *xorm.Engine
+
+func main() {
+	x, err := xorm.NewEngine("mysql", "root:wang1234@/symbol?charset=utf8")
+
+	if err = x.Sync2(new(Person)); err != nil {
+		log.Fatalf("Fail to sync database: %v\n", err)
+	}
+
+	person := Person{
+		Id: 1,
+		Name: "wmg",
+		Salary: decimal.NewFromFloat(100),
+	}
+
+	i, err := x.Insert(&person)
+	fmt.Println(i, err)
+
+	a := &Person{}
+	has, err := x.Where(map[string]interface{}{"id": 1, "name": "wmg"}).NoAutoCondition(true).Get(a)
+	fmt.Println(has, a, err)
+}
