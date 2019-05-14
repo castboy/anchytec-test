0397d2919ea6e1883bed23094bc98dbf443efc36
castboy <482819895@qq.com>
2019-05-14 11:45:48 +0800

    functional_options2.go

---
+++ functional_options2.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+type One struct {
+	Name string
+	Age int
+	Male bool
+}
+
+func Male(t *One) error {
+	t.Male = true
+	return nil
+}
+
+func Age(age int) func(t *One) error {
+	return func(t *One) error {
+		return t.setAge(age)
+	}
+}
+
+func (t *One) setAge(age int) error {
+	t.Age = age
+	return nil
+}
+
+func NewOne(name string, options ...func(t *One) error) (*One, error) {
+	one := One{Name:name}
+
+	for _, option := range options {
+		option(&one)
+	}
+
+	return &one, nil
+}
+
+func main() {
+	one, _ := NewOne("wmg")
+	fmt.Println(one)
+
+	one2, _ := NewOne("wmg", Male, Age(28))
+	fmt.Println(one2)
+}
