6f94adea6e35f0417e123cdf642adad5ee93267a
castboy <482819895@qq.com>
2019-05-23 15:36:11 +0800

    type.go

---
+++ type.go
@@ -0,0 +1,25 @@
+package main
+
+type I int
+type AliasI = int
+
+func main() {
+	v := 100
+	var i I = v    // 报错
+	var d AliasI = v    // 不报错
+	var z int = d       // 不报错
+	TypeAlias(d)        // 不报错
+
+
+
+	Type(100)   // 不报错
+	Type(v)        // 报错
+	Type(I(v))     // 不报错
+
+	var mv I = 10  // 不报错
+	Type(mv)	   // 不报错
+}
+
+func Type(i I) {}
+
+func TypeAlias(i int) {}
