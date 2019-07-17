c5b5950833a3bad3429e2bb3440690ad9d2e6359
castboy <482819895@qq.com>
2019-07-17 17:27:29 +0800

    retry.go

---
+++ retry.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/Rican7/retry"
+	"github.com/Rican7/retry/strategy"
+	"github.com/Rican7/retry/backoff"
+	"errors"
+	"fmt"
+	"time"
+)
+
+func main() {
+	action := func(attempt uint) error {
+		fmt.Println(time.Now())
+		return errors.New("basic error")
+	}
+
+	const duration = time.Second
+	const Incremental = time.Second
+	incremental := backoff.Incremental(duration, Incremental)
+	strategy1 := strategy.Backoff(incremental)
+
+	strategy2 := strategy.Limit(5)
+
+	err := retry.Retry(action, strategy1, strategy2)
+	fmt.Println(err)
+}
