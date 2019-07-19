c6dfb10e8596e3d99ab9b4cc9fc82e43c22a1911
castboy <482819895@qq.com>
2019-07-19 11:47:02 +0800

    no message

--- gomonkey_monkey.go
+++ gomonkey_monkey.go
@@ -4,13 +4,17 @@ import (
 	"bou.ke/monkey"
 	"fmt"
 	"github.com/agiledragon/gomonkey"
+	"github.com/pkg/errors"
 	"reflect"
 )
 
 // note: go build -gcflags=all=-l gomonkey_monkey.go
 
+var num = 100
+
 func main() {
 	// monkey below.
+	// Patch
 	patchGuard := monkey.Patch(target, func(a string) string {
 		return "gomonkey"
 	})
@@ -22,7 +26,7 @@ func main() {
 	s = target("a")
 	fmt.Println(s == "a")
 
-
+	// PatchInstanceMethod
 	var myInt MyInt = 10
 	patchGuard = monkey.PatchInstanceMethod(reflect.TypeOf(myInt), "Method", func(myI MyInt) int {
 		return int(myI) + 1
@@ -36,6 +40,7 @@ func main() {
 	fmt.Println(v == 10)
 
 	// gomonkey below.
+	// ApplyFunc
 	patches := gomonkey.ApplyFunc(target, func(a string) string {
 		return "monkey"
 	})
@@ -46,7 +51,43 @@ func main() {
 	patches.Reset()
 	s = target("a")
 	fmt.Println(s == "a")
+	// ApplyGlobalVar
+	patches = gomonkey.ApplyGlobalVar(&num, 150)
+	fmt.Println(num == 150)
+	patches.Reset()
+	fmt.Println(num == 100)
 
+	// ApplyFuncSeq
+	outputs := []gomonkey.OutputCell{
+		{Values: gomonkey.Params{"output1", nil}, Times: 2},
+		{Values: gomonkey.Params{"output2", errors.New("new func seq")}},
+	}
+	gomonkey.ApplyFuncSeq(funcSeq, outputs)
+	s1, err := funcSeq()
+	fmt.Println(s1 =="output1", err == nil)
+	s1, err = funcSeq()
+	fmt.Println(s1 =="output1", err == nil)
+	s1, err = funcSeq()
+	fmt.Println(s1 =="output2", err.Error())
+
+	patchPairs := [][2]interface{}{
+		{
+			target,
+			func(a string) string {return "gomonkey"},
+		},
+		{
+			funcSeq,
+			func() (string, error) { return "", errors.New("new func seq")},
+		},
+	}
+
+	for i := range patchPairs {
+		gomonkey.ApplyFunc(patchPairs[i][0], patchPairs[i][1])
+	}
+	s = target("")
+	fmt.Println(s == "gomonkey")
+	s, err = funcSeq()
+	fmt.Println(s == "", err)
 }
 
 func target(a string) string {
@@ -58,3 +99,6 @@ func (my MyInt) Method() int {
 	return int(my)
 }
 
+func funcSeq() (string, error) {
+	return "", errors.New("func seq")
+}
