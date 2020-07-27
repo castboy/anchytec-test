05b3b8decf17a5782275a826b543221218a25254
castboy <482819895@qq.com>
2020-07-27 19:07:06 +0800

    error/ add args_trade.go

--- error/args.go
+++ error/args.go
@@ -1,56 +1,11 @@
 package error
 
-type reqType errCodeSub
-type tradeReq errCodeSubSub
+type argsType errCodeSub
 
 const (
-	Trade reqType = iota + 1
+	Trade argsType = iota + 1
 )
 
-var reqTypeMsg = map[reqType]string{
+var argsTypeMsg = map[argsType]string{
 	Trade: "Trade",
-}
-
-const (
-	TypeErr tradeReq = iota + 1
-	LoginErr
-	TicketErr
-	CmdErr
-	SymbolErr
-	LeverageErr
-	VolumeErr
-	TpErr
-	SlErr
-	CommentErr
-	ClientPriceErr
-	ClientTimeErr
-	PendingPriceErr
-	ExpirationErr
-	AmountErr
-)
-
-var tradeArgsMsg = map[tradeReq]string{
-	TypeErr:         "type",
-	LoginErr:        "login",
-	TicketErr:       "ticket",
-	CmdErr:          "cmd",
-	SymbolErr:       "symbol",
-	LeverageErr:     "leverage",
-	VolumeErr:       "volume",
-	TpErr:           "tp",
-	SlErr:           "sl",
-	CommentErr:      "comment",
-	ClientPriceErr:  "client-price",
-	ClientTimeErr:   "client-time",
-	PendingPriceErr: "pending-price",
-	ExpirationErr:   "expiration",
-	AmountErr:       "amount",
-}
-
-type ArgsErrer struct {
-	*baseErrer
-}
-
-func NewArgsErrer(opType reqType, subSub errCodeSubSub) *ArgsErrer {
-	return &ArgsErrer{}
 }
--- error/args_trade.go
+++ error/args_trade.go
@@ -1,3 +1,68 @@
 package error
 
+import "fmt"
 
+type tradeArg errCodeSubSub
+
+const (
+	TypeErr tradeArg = iota + 1
+	LoginErr
+	TicketErr
+	CmdErr
+	SymbolErr
+	LeverageErr
+	VolumeErr
+	TpErr
+	SlErr
+	CommentErr
+	ClientPriceErr
+	ClientTimeErr
+	PendingPriceErr
+	ExpirationErr
+	AmountErr
+)
+
+var tradeArgsMsg = map[tradeArg]string{
+	TypeErr:         "type",
+	LoginErr:        "login",
+	TicketErr:       "ticket",
+	CmdErr:          "cmd",
+	SymbolErr:       "symbol",
+	LeverageErr:     "leverage",
+	VolumeErr:       "volume",
+	TpErr:           "tp",
+	SlErr:           "sl",
+	CommentErr:      "comment",
+	ClientPriceErr:  "client-price",
+	ClientTimeErr:   "client-time",
+	PendingPriceErr: "pending-price",
+	ExpirationErr:   "expiration",
+	AmountErr:       "amount",
+}
+
+type TradeArgsErrer struct {
+	*baseErrer
+}
+
+func NewTradeArgsErrer(errOrigin error, reqType argsType, tradeArg tradeArg, comment ...Comment) *TradeArgsErrer {
+	base := newErr()
+	base.setCode(argsErr)
+	base.setCodeSub(errCodeSub(reqType))
+	base.setCodeSubSub(errCodeSubSub(tradeArg))
+	base.setOriginErr(errOrigin)
+
+	f := getCallFunc(newDeep)
+	base.appendCallFunc(f)
+
+	base.appendComment(comment...)
+
+	return &TradeArgsErrer{base}
+}
+
+func (i *TradeArgsErrer) encodeErrCode() string {
+	return fmt.Sprintf("%s/%s/%s", errCodeMsg[i.code], argsTypeMsg[argsType(i.codeSub)], tradeArgsMsg[tradeArg(i.codeSubSub)])
+}
+
+func (i *TradeArgsErrer) Error() string {
+	return encodeErrMsg(i)
+}
