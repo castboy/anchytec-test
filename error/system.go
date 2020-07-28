59935bf0b4cbf34f75fea64df188fd6f121c07bd
castboy <482819895@qq.com>
2020-07-28 15:53:50 +0800

    error/; add system error

--- error/system.go
+++ error/system.go
@@ -1,5 +1,30 @@
 package error
 
-import "anchytec/error/constant"
+import (
+	"anchytec/error/constant"
+	"anchytec/error/constant/system"
+)
 
-var errMsgSystem = map[constant.ErrCodeSub]string{}
+type SystemErrer struct {
+	*baseErrer
+}
+
+func NewSystemErrer(errOrigin error, Type system.Type) *SystemErrer {
+	base := newErr()
+	base.init(errOrigin, constant.SystemErr, constant.ErrCodeSub(Type), constant.ErrCodeSubSub(0))
+
+	return &SystemErrer{base}
+}
+
+func (i *SystemErrer) getErrCodeSubMsg() string {
+	return system.SystemTypeMsg[system.Type(i.codeSub)]
+}
+
+func (i *SystemErrer) getErrCodeSubSubMsg() string {
+	return ""
+}
+
+func (i *SystemErrer) Error() string {
+	return encodeErrMsg(i)
+}
+
